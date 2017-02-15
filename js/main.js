9b53f3241028a19c83fef77af12b7622d56357e0
trop0008 <trop0008@algonquinlive.com>
2017-02-15 12:52:40 -0500

    changing script

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", function(){
 //when dom is loaded...
 })
-
+var globalvar="Hello";
 window.addEventListener('push', function(ev){
 //determine the page
 let contentDiv = ev.currentTarget.document.querySelector(".content");
+    
 let id = contentDiv.id; //contentDiv.getAttribute("id");
 switch(id){
 case "x":
 //do something for page x
 break;
 case "one":
+   //     id.innerHTML=globalvar;
+        globalvar= globalvar+"1";
 //do something for the home page 
-alert("PAGE ONE STUFF");
+        alert(globalvar);
+//alert("PAGE ONE STUFF");
 break;
 case "two":
 //do something for the contacts page 
+                id.innerHTML=globalvar;
+        globalvar= globalvar+"2";
+
 alert("PAGE TWO STUFF");
 break;
 default:
