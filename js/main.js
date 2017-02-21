30a997ea2212cf53f1fab14232316152b998a727
trop0008 <trop0008@algonquinlive.com>
2017-02-20 22:47:01 -0500

    fixed ipad bug

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ let url = "https://griffis.edumedia.ca/mad9022/tundra/get.profiles.php?gender=" 
 let profiles = [];
 let imgurl = "";
 let swipeCheck = false;
-
-
-
 window.addEventListener('push', function (ev) {
     //determine the page
     let contentDiv = ev.currentTarget.document.querySelector(".content");
@@ -16,7 +13,6 @@ window.addEventListener('push', function (ev) {
         //do something for page x
         break;
     case "one":
-       
         touchAction();
         if (profiles.length < 3) {
             getProfiles();
@@ -27,7 +23,6 @@ window.addEventListener('push', function (ev) {
         break;
     case "two":
         //do something for the contacts page 
-        
         break;
     default:
         //do the home page thing
@@ -55,7 +50,6 @@ function swipeFunction(event) {
     console.log(event.detail.data[0].currentDirection);
     console.log(event.detail.data[0].velocity);
     if (event.detail.data[0].currentDirection >= 150 && event.detail.data[0].currentDirection <= 210) {
-        
         swipeLeft();
     }
     else if (event.detail.data[0].currentDirection >= 330 || event.detail.data[0].currentDirection <= 30) {
@@ -66,75 +60,89 @@ function swipeFunction(event) {
 }
 
 function getProfiles() {
-    if (profiles.length == 0){
+    if (profiles.length == 0) {
         document.getElementById('myCard').innerHTML = "<h1 >Loading</h1>";
     }
-    try{
-        fetch("https://griffis.edumedia.ca/mad9022/tundra/get.profiles.php").then(function (response) {
-        return response.json();
-    }).then(function (data) {
-        imgurl = "https:" + decodeURIComponent(data.imgBaseURL);
-        //console.log(data.imgBaseURL);
-        //console.log(imgurl);
-        profiles = profiles.concat(data.profiles);
-        console.log(profiles);
-        showProfile();
-        /*profiles.forEach(function(person){
-          let img = document.createElement("img");
-          let p = document.createElement("p");
-          let name = "".concat(person.first, " ", person.last);
-          img.src = imgurl + person.avatar;
-          p.appendChild(img);
-          p.innerHTML += " " + name;
-          p.addEventListener('click', function(ev){
-            alert('you clicked' + ev.currentTarget.textContent)
-          });
-          document.getElementById("mycard").appendChild(p);
-        });*/
-    }).catch(function (err) {
-        document.getElementById('myCard').innerHTML = "<h1 >failed</h1>";
+    try {
+        fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            imgurl = "https:" + decodeURIComponent(data.imgBaseURL);
+            //console.log(data.imgBaseURL);
+            //console.log(imgurl);
+            profiles = profiles.concat(data.profiles);
+            console.log(profiles);
+            showProfile();
+            /*profiles.forEach(function(person){
+              let img = document.createElement("img");
+              let p = document.createElement("p");
+              let name = "".concat(person.first, " ", person.last);
+              img.src = imgurl + person.avatar;
+              p.appendChild(img);
+              p.innerHTML += " " + name;
+              p.addEventListener('click', function(ev){
+                alert('you clicked' + ev.currentTarget.textContent)
+              });
+              document.getElementById("mycard").appendChild(p);
+            });*/
+        }).catch(function (err) {
+            document.getElementById('myCard').innerHTML = "<h1 >failed</h1>";
+            //document.getElementById('myCard').innerHTML = err.message;
+        });
+    }
+    catch (err) {
         //document.getElementById('myCard').innerHTML = err.message;
-        
-                    
-    });
-    } catch(err) {
-    document.getElementById('myCard').innerHTML = err.message;
-}
-    
+
+        function reqListener() {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+            profiles = profiles.concat(data.profiles);
+             imgurl = "https:" + decodeURIComponent(data.imgBaseURL);
+            showProfile();
+        }
+
+        function reqError(err) {
+            document.getElementById('myCard').innerHTML = 'Fetch Error :-S', err;
+        }
+        var oReq = new XMLHttpRequest();
+        oReq.onload = reqListener;
+        oReq.onerror = reqError;
+        oReq.open('get', url, true);
+        oReq.send();
+    }
 }
 
 function swipeLeft() {
-    if(swipeCheck == true){
-    let profile = profiles.shift();
-     swipeCheck = false;
-    document.getElementById('myCard').innerHTML = "<h1 class='delete'>Profile Deleted<h1>";
-    let preloadimg = new Image();
-   
-    preloadimg.src = imgurl + profiles[0].avatar;
-    if (profiles.length < 3) {
-        getProfiles();
-    }
-    setTimeout(showProfile, 1000);
-    console.log(profile);
-    console.log(profiles);
+    if (swipeCheck == true) {
+        let profile = profiles.shift();
+        swipeCheck = false;
+        document.getElementById('myCard').innerHTML = "<h1 class='delete'>Profile Deleted<h1>";
+        let preloadimg = new Image();
+        preloadimg.src = imgurl + profiles[0].avatar;
+        if (profiles.length < 3) {
+            getProfiles();
+        }
+        setTimeout(showProfile, 1000);
+        console.log(profile);
+        console.log(profiles);
     }
 }
 
 function showProfile() {
-    swipeCheck = true;
     let cardprofile = document.getElementById('myCard');
     let img = document.createElement("img");
     let h2 = document.createElement("h2");
     let name = "".concat(profiles[0].first, " ", profiles[0].last);
     img.src = imgurl + profiles[0].avatar;
-    
     h2.innerHTML += " " + name;
+    let p = document.createElement("p");
+       p.innerHTML= profiles[0].distance;
     cardprofile.innerHTML = "";
     cardprofile.appendChild(img);
     cardprofile.appendChild(h2);
+    cardprofile.appendChild(p);
+    swipeCheck = true;
 }
-
-
 document.addEventListener("DOMContentLoaded", function () {
     //when dom is loaded...
     let contentDiv = document.querySelector(".content");
@@ -144,7 +152,6 @@ document.addEventListener("DOMContentLoaded", function () {
         //do something for page x
         break;
     case "one":
-               
         touchAction();
         if (profiles.length < 3) {
             getProfiles();
@@ -154,7 +161,6 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         break;
     case "two":
-
         // page two
         break;
     default:
